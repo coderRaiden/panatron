646d19827f17ebd6ce734f16ee2f85f64b103695
coderRaiden <tardevishal7@gmail.com>
2023-09-04 20:19:55 +0530

    "Page Routing done"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import React from "react";
 import "./App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+//Create a page routing.
+//We have created Layout in which Navbar and Footer will be common for Home,Products and Product pages.
+//For this we have used "Outlet" from roter-dom.This allow change Outlet page.
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // Childrens for oulet.
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+  return <div>Category</div>;
+};
+
+export default Products;
