7fbf71321dcf49efe6243718a6a6571c2fdf1885
coderRaiden <tardevishal7@gmail.com>
2023-09-05 22:12:13 +0530

    "Navbar completed"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
 //Create a page routing.
 //We have created Layout in which Navbar and Footer will be common for Home,Products and Product pages.
