dec228886f9255c05f4000c0c4054b8e47edfa87
coderRaiden <tardevishal7@gmail.com>
2023-09-29 21:19:03 +0530

    card completed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,13 +7,16 @@ const Card = ({ item }) => {
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
+          {item.isNew && <span>New Season</span>}
           <img src={item.img} alt="img" className="mainImg" />
           <img src={item.img2} alt="img2" className="secondImg" />
         </div>
-        <h2>{item.title} </h2>
-        <div className="prices">
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
+        <div className="pAdjust">
+          <h2>{item.title} </h2>
+          <div className="prices">
+            <h3>${item.oldPrice}</h3>
+            <h3>${item.price}</h3>
+          </div>
         </div>
       </div>
     </Link>
