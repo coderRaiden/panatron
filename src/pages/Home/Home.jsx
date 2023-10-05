212d938897aeda68a9d1aae8b77d01f3e1de960a
coderRaiden <tardevishal7@gmail.com>
2023-10-06 00:09:24 +0530

    Categories section finished

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
 import FeaturedProducts from "../../components/FeauturedProducts/FeaturedProducts";
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
       <FeaturedProducts type="Featured" />
+      <Categories />
       <FeaturedProducts type="Trending" />
       {/* We are using "type" prop which help to create same feature */}
     </div>
