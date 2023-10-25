6b7aff3439bdabea7efa8e219740b2e0d4041d80
coderRaiden <tardevishal7@gmail.com>
2023-10-25 23:48:06 +0530

    Categories parially done

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./Products.scss";
+import fet_coat from "../../pics/feature/fet_coat.jpg";
 
 const Products = () => {
-  return <div className="prodcuts">Category</div>;
+  return (
+    <div className="prodcuts">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Skirts</label>
+          </div>
+
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+
+        <div className="filterItem">
+          <h2>Filter By price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort By</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" />
+            <label htmlFor="desc">Price (Highest First)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img className="catImg" src={fet_coat} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
