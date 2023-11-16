cef86de08864620e2e5cdab7ea3ba99c3a59344c
coderRaiden <tardevishal7@gmail.com>
2023-11-16 14:56:51 +0530

    Products completed

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./List.scss";
+import Card from "../Card/Card";
+import list_hat from "../../pics/list/list_hat.jpg";
+import list_skirt from "../../pics/list/list_skirt.jpg";
+import list_fullSleeve from "../../pics/list/list_full sleeve.jpg";
+import list_coat from "../../pics/list/list_coat.jpg";
 
 const List = () => {
-  return <div className="list">List</div>;
+  const data = [
+    {
+      id: 1,
+      img: list_fullSleeve,
+      img2: list_fullSleeve,
+      title: "Long sleeve Graphic T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      priceL: 12,
+    },
+    {
+      id: 2,
+      img: list_coat,
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      priceL: 12,
+    },
+
+    {
+      id: 3,
+      img: list_skirt,
+      title: "Skirt",
+      isNew: true,
+      oldPrice: 19,
+      priceL: 12,
+    },
+    {
+      id: 4,
+      img: list_hat,
+      title: "Hat",
+      isNew: true,
+      oldPrice: 19,
+      priceL: 12,
+    },
+  ];
+
+  return (
+    <div className="list">
+      {data.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default List;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Products.scss";
 import fet_coat from "../../pics/feature/fet_coat.jpg";
+import List from "../../components/List/List";
+import { useParams } from "react-router-dom";
 
 const Products = () => {
+  //The useParams hook returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>
+  const catId = parseInt(useParams().id);
+
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+
   return (
-    <div className="prodcuts">
+    <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
@@ -28,25 +36,43 @@ const Products = () => {
           <h2>Filter By price</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={1000} />
-            <span>1000</span>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort By</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
             <label htmlFor="asc">Price (Lowest First)</label>
           </div>
 
           <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" />
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
             <label htmlFor="desc">Price (Highest First)</label>
           </div>
         </div>
       </div>
       <div className="right">
         <img className="catImg" src={fet_coat} alt="" />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} />
       </div>
     </div>
   );
